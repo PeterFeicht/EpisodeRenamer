c2e90210ed1fc89a5531c4e352b2eefa634e4e17
Peter Feichtinger <shippo@gmx.at>
2011-03-02 19:25:14 +0000

    EpisodeRenamer: -> Moved project and solution files to trunk directory.

---
+++ EpisodeRenamer/ChangePrefixForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace EpisodeRenamer
+{
+	partial class ChangePrefixForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnOK = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.txtSeasonPrefix = new System.Windows.Forms.TextBox();
+			this.txtEpisodePrefix = new System.Windows.Forms.TextBox();
+			this.txtSeparator = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.lblExample = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.numDigits = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numDigits)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(260, 56);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Here you can change the prefixes for season and episode numbers as well as the se" +
+				"parator used to separate the name of the show, the numbers and the name.";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 65);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Season prefix:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(118, 65);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(76, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Episode prefix:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 143);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(50, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Example:";
+			// 
+			// btnOK
+			// 
+			this.btnOK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btnOK.Location = new System.Drawing.Point(58, 201);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 4;
+			this.btnOK.Text = "&OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnCancel.Location = new System.Drawing.Point(151, 201);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 5;
+			this.btnCancel.Text = "&Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// txtSeasonPrefix
+			// 
+			this.txtSeasonPrefix.Location = new System.Drawing.Point(12, 81);
+			this.txtSeasonPrefix.Name = "txtSeasonPrefix";
+			this.txtSeasonPrefix.Size = new System.Drawing.Size(100, 20);
+			this.txtSeasonPrefix.TabIndex = 0;
+			this.txtSeasonPrefix.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+			// 
+			// txtEpisodePrefix
+			// 
+			this.txtEpisodePrefix.Location = new System.Drawing.Point(118, 81);
+			this.txtEpisodePrefix.Name = "txtEpisodePrefix";
+			this.txtEpisodePrefix.Size = new System.Drawing.Size(100, 20);
+			this.txtEpisodePrefix.TabIndex = 1;
+			this.txtEpisodePrefix.Text = "x";
+			this.txtEpisodePrefix.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+			// 
+			// txtSeparator
+			// 
+			this.txtSeparator.Location = new System.Drawing.Point(12, 120);
+			this.txtSeparator.Name = "txtSeparator";
+			this.txtSeparator.Size = new System.Drawing.Size(100, 20);
+			this.txtSeparator.TabIndex = 2;
+			this.txtSeparator.Text = " - ";
+			this.txtSeparator.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 104);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(56, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Separator:";
+			// 
+			// lblExample
+			// 
+			this.lblExample.AutoSize = true;
+			this.lblExample.Location = new System.Drawing.Point(12, 162);
+			this.lblExample.Name = "lblExample";
+			this.lblExample.Size = new System.Drawing.Size(150, 13);
+			this.lblExample.TabIndex = 10;
+			this.lblExample.Text = "Series - 01x05 - Episode name";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(118, 104);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(86, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Number of digits:";
+			// 
+			// numDigits
+			// 
+			this.numDigits.Location = new System.Drawing.Point(118, 121);
+			this.numDigits.Maximum = new decimal(new int[ ] {
+            4,
+            0,
+            0,
+            0});
+			this.numDigits.Minimum = new decimal(new int[ ] {
+            1,
+            0,
+            0,
+            0});
+			this.numDigits.Name = "numDigits";
+			this.numDigits.Size = new System.Drawing.Size(100, 20);
+			this.numDigits.TabIndex = 3;
+			this.numDigits.Value = new decimal(new int[ ] {
+            2,
+            0,
+            0,
+            0});
+			this.numDigits.ValueChanged += new System.EventHandler(this.numDigits_ValueChanged);
+			// 
+			// ChangePrefixForm
+			// 
+			this.AcceptButton = this.btnOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnCancel;
+			this.ClientSize = new System.Drawing.Size(284, 236);
+			this.Controls.Add(this.numDigits);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.lblExample);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtSeparator);
+			this.Controls.Add(this.txtEpisodePrefix);
+			this.Controls.Add(this.txtSeasonPrefix);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "ChangePrefixForm";
+			this.ShowInTaskbar = false;
+			this.Text = "Change prefixes";
+			this.Load += new System.EventHandler(this.ChangePrefixForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numDigits)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.TextBox txtSeasonPrefix;
+		private System.Windows.Forms.TextBox txtEpisodePrefix;
+		private System.Windows.Forms.TextBox txtSeparator;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label lblExample;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.NumericUpDown numDigits;
+	}
+}
---
+++ EpisodeRenamer/EpisodeEntry.cs
@@ -0,0 +1,541 @@
+﻿using System.IO;
+using System;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Text;
+
+namespace EpisodeRenamer
+{
+	class EpisodeEntry
+	{
+		#region types, fields, ctors
+
+		/// <summary>
+		/// Specifies the type of entry.
+		/// </summary>
+		public enum EntryType
+		{
+			/// <summary>
+			/// Neither an old nor a new filename have been set.
+			/// </summary>
+			None,
+			/// <summary>
+			/// The old filenme does not contain an episode name, but contains season and episode information and a new filename is set.
+			/// </summary>
+			Green,
+			/// <summary>
+			/// The old filename does not contain season or episode information but a new filename is set.
+			/// </summary>
+			Red,
+			/// <summary>
+			/// The old filename contains season and episode information as well as an episode name and a new name is set.
+			/// </summary>
+			Yellow,
+			/// <summary>
+			/// No new episode name is set.
+			/// </summary>
+			Gray
+		}
+
+		string oldFilename = "";
+		string newFilename = "";
+		EntryType typeCache;
+		bool typeCacheDirty = true;
+
+		/// <summary>
+		/// Initializes a new instance with <see cref="Enabled"/> set to <value>false</value> and empty filenames.
+		/// </summary>
+		private EpisodeEntry()
+		{
+			Enabled = false;
+			NewNameString = "";
+			EpisodeNumber = new Point(-1, -1);
+			Moved = false;
+			FilePath = "";
+			MoveException = null;
+		}
+
+		/// <summary>
+		/// Initializes a new instance with <see cref="Enabled"/> set to <value>false</value>, the old filename specified and an empty new filename.
+		/// </summary>
+		/// <param name="oldFilename">The old filename to use for this instance.</param>
+		public EpisodeEntry(string oldFilename)
+			: this()
+		{
+			OldFilename = oldFilename;
+		}
+
+		/// <summary>
+		/// Initializes a new instance with <see cref="Enabled"/> set to <value>true</value> and the old and new filenames specified.
+		/// </summary>
+		/// <param name="oldFilename">The old filename to use for this instance</param>
+		/// <param name="newFilename">The new filename to use for this instance</param>
+		public EpisodeEntry(string oldFilename, string newFilename)
+			: this(oldFilename, newFilename, true)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance with <see cref="Enabled"/> and the filenames set to the values specified.
+		/// </summary>
+		/// <param name="oldFilename">The old filename to use for this instance</param>
+		/// <param name="newFilename">The new filename to use for this instance</param>
+		/// <param name="enabled">The value to use for <see cref="Enabled"/>.</param>
+		public EpisodeEntry(string oldFilename, string newFilename, bool enabled)
+			: this()
+		{
+			OldFilename = oldFilename;
+			NewFilename = newFilename;
+			Enabled = enabled;
+		}
+
+		#endregion types, fields, ctors
+
+
+		#region properties
+
+		/// <summary>
+		/// Gets or sets the old filename, must be an absolute path that exists.
+		/// </summary>
+		/// <exception cref="System.ArgumentException">Thrown, if the specified file does not exist.</exception>
+		/// <remarks>No exception is thrown if the path supplied is not absolute. However, there is no guarantee on which working directory 
+		/// will be used to calculate the absolute path.</remarks>
+		public string OldFilename
+		{
+			get
+			{
+				return oldFilename;
+			}
+			set
+			{
+				if(!File.Exists(value))
+					throw new ArgumentException("The file specified does not exist.");
+
+				oldFilename = Path.GetFileName(value);
+				FilePath = Path.GetDirectoryName(Path.GetFullPath(value));
+				typeCacheDirty = true;
+
+				string[ ] parts = SplitFilename(oldFilename);
+
+				if(parts.Length >= 2)
+				{
+					Series = parts[0];
+
+					if(parts.Length > 2)
+						for(int j = 2; j < parts.Length; j++)
+							parts[1] += " " + parts[j];
+					
+					EpisodeNumber = GetEpisodeInfo(parts[1]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the new filename. Must be a filename without path information.
+		/// </summary>
+		public string NewFilename
+		{
+			get
+			{
+				return newFilename;
+			}
+			set
+			{
+				string temp = Path.GetFileName(value);
+				if(string.IsNullOrEmpty(temp))
+					throw new ArgumentException("The specified value is not a proper filename.");
+				else
+				{
+					newFilename = temp;
+					typeCacheDirty = true;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the path containing the file to be renamed.
+		/// </summary>
+		public string FilePath
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets or sets whether the rename action will be performed if <see cref="PerformRename"/> is called.
+		/// </summary>
+		public bool Enabled
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets whether the file has already been renamed.
+		/// </summary>
+		public bool Moved
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the Exception that occurred when trying to move the file in case <see cref="PerformRename"/> returned <c>false</c>, or <c>null</c> if no exception occurred.
+		/// </summary>
+		public Exception MoveException
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets or sets the line of episode names corresponding to this rename action
+		/// </summary>
+		public string NewNameString
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets the episode number extracted from the old filename.
+		/// </summary>
+		/// <remarks>The X and Y coordinates of the Point correspond to the Season and Episode numbers respectively.
+		/// In case the episode number cannot be extracted from the old filename, the coordinates are set to <c>-1</c>.</remarks>
+		public Point EpisodeNumber
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the name of the series the file belongs to, extracted from the old filename.
+		/// </summary>
+		/// <remarks>In case the name cannot be extracted the value is set to <c>null</c>.</remarks>
+		public string Series
+		{
+			get;
+			private set;
+		}
+
+		#endregion properties
+
+
+		#region methods
+
+		/// <summary>
+		/// Renames the file in <see cref="OldFilename"/> to the name in <see cref="NewFilename"/> if <see cref="Enabled"/> is set to <value>true</value>.
+		/// </summary>
+		/// <returns><c>true</c> if the rename was successful or <see cref="Enabled"/> is set to <c>false</c>, otherwise <c>false</c>.</returns>
+		public bool PerformRename()
+		{
+			if(!Enabled || Moved)
+				return true;
+
+			Enabled = false;
+
+			try
+			{
+				FileInfo f = new FileInfo(Path.Combine(FilePath, oldFilename));
+				f.MoveTo(Path.Combine(FilePath, newFilename));
+			}
+			catch(Exception ex)
+			{
+				MoveException = ex;
+				return false;
+			}
+
+			MoveException = null;
+			Moved = true;
+			return true;
+		}
+
+		/// <summary>
+		/// Gets the type of this episode entry based on old and new filename, separator and prefixes.
+		/// </summary>
+		/// <returns>The determined entry type, it is not guaranteed to match the real type, only a guess.</returns>
+		/// <remarks>Note that the values of <see cref="EpisodeEntry.Separator"/>, <see cref="EpisodeEntry.SeasonPrefix"/> and 
+		/// <see cref="EpisodeEntry.EpisodePrefix"/> do affect the operation of this method.</remarks>
+		public EntryType GetEntryType()
+		{
+			if(typeCacheDirty)
+			{
+				if(string.IsNullOrEmpty(newFilename))
+				{
+					if(string.IsNullOrEmpty(oldFilename))
+						typeCache = EntryType.None;
+					else
+						typeCache = EntryType.Gray;
+				}
+				else
+				{
+					string name = Path.GetFileNameWithoutExtension(oldFilename);
+
+					string[ ] parts = SplitFilename(name);
+
+					switch(parts.Length)
+					{
+						case 1:
+							// Old filename does not contain season or episode information that could be matched
+							typeCache = EntryType.Red;
+							break;
+
+						case 2:
+							// Try splitting the second part in case the name was matched by generic season number expressions
+							if(string.IsNullOrWhiteSpace(EpisodePrefix))
+								parts = Regex.Split(parts[1], RegexEpisodeNumberMatchString, DefaultRegexOptions);
+							else
+								parts = RemoveEmptyStrings(Regex.Split(parts[1], "(.*" + Regex.Escape(EpisodePrefix) + "[0-9]{1,3})([^0-9]+)", DefaultRegexOptions));
+
+							// Second part may contain episode name
+							if((parts.Length == 2) && string.IsNullOrEmpty(parts[0]))
+								typeCache = EntryType.Yellow;
+							else
+								typeCache = EntryType.Green;
+
+							break;
+
+						case 3:
+							// Old filename may already contain an episode name
+							typeCache = EntryType.Yellow;
+							break;
+
+						default:
+							// Structure of the old filename could not be determined, so make it Red to encourage the user to check the filename
+							typeCache = EntryType.Yellow;
+							break;
+					}
+				}
+
+				typeCacheDirty = false;
+			}
+
+			return typeCache;
+		}
+
+		/// <summary>
+		/// Gets the type of this episode entry based on old and new filename, separator and prefixes.
+		/// </summary>
+		/// <param name="forceRebuild">Specifies, if the cached value should be recalculated, even if nothing has changed.</param>
+		/// <returns>The determined entry type, it is not guaranteed to match the real type, only a guess</returns>
+		/// <remarks>The type is not automatically recalculated if the static properties <see cref="Separator"/>, 
+		/// <see cref="EpisodePrefix"/> or <see cref="SeasonPrefix"/> are changed. Because these properties affect the 
+		/// behaviour of GetEntryType, it might be necessary to call it with <paramref name="forceRebuild"/> set to <c>true</c> 
+		/// to determine the new entry type.</remarks>
+		public EntryType GetEntryType(bool forceRebuild)
+		{
+			typeCacheDirty = typeCacheDirty || forceRebuild;
+
+			return GetEntryType();
+		}
+
+		/// <summary>
+		/// Tries to split a filename into parts containing separate series name, episode information and episode name.
+		/// </summary>
+		/// <param name="name">The filename to split.</param>
+		/// <returns>An array containing all the parts of the filename.</returns>
+		/// <remarks>All the parts concatenated to a single string may not give the original filename, as parts are removed when matching 
+		/// possible separators.</remarks>
+		private string[ ] SplitFilename(string name)
+		{
+			string[ ] parts = name.Split(new string[ ] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+			if(!string.IsNullOrWhiteSpace(SeasonPrefix) && (parts.Length == 1))
+				parts = RemoveEmptyStrings(Regex.Split(name, "(" + SeasonPrefix + ")([0-9].*)", DefaultRegexOptions));
+			if(parts.Length == 1)
+				parts = RemoveEmptyStrings(Regex.Split(name, @"\.(s[0-9].*)", DefaultRegexOptions));
+			if(parts.Length == 1)
+				parts = RemoveEmptyStrings(Regex.Split(name, " (s[0-9].*)", DefaultRegexOptions));
+
+			return parts;
+		}
+
+		#endregion methods
+
+
+		#region static members
+		
+		static int episodeNumberDigits;
+
+		/// <summary>
+		/// A regular expression match string used to match episode information in filenames.
+		/// </summary>
+		public static readonly string RegexEpisodeNumberMatchString = @"(?<![0-9])[0-9]{1,3}[^0-9]+[0-9]{0,3}(?![0-9])";
+
+		/// <summary>
+		/// The default options for regular expression matching used internally.
+		/// </summary>
+		public static readonly RegexOptions DefaultRegexOptions = RegexOptions.Singleline | RegexOptions.IgnoreCase;
+
+		static EpisodeEntry()
+		{
+			Separator = " - ";
+			EpisodePrefix = "x";
+			SeasonPrefix = "";
+			EpisodeNumberDigits = 2;
+		}
+
+		/// <summary>
+		/// Gets or sets the string used to separate the series name from the season prefix and the episode number from the episode name.
+		/// The default value is <c>" - "</c>.
+		/// </summary>
+		/// <remarks>This value is also used when determining the type of the entry.
+		/// Common values are <c>" - "</c>, <c>"."</c> or <c>" "</c>.</remarks>
+		public static string Separator
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the string used as a prefix to the episode number.
+		/// The default value is <c>"x"</c>.
+		/// </summary>
+		/// <remarks>This value is also used when determining the type of the entry.
+		/// Common values are <c>"x"</c> or <c>"e"</c>.</remarks>
+		public static string EpisodePrefix
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the string used as a prefix to the season number.
+		/// The default value is <c>""</c>.
+		/// </summary>
+		/// <remarks>This value is also used when determining the type of the entry.
+		/// Common values are <c>""</c> or <c>"s"</c>.</remarks>
+		public static string SeasonPrefix
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the minimum number of digits to use for epipsode and season numbers when formatting filenames.
+		/// The default value is <c>2</c>
+		/// </summary>
+		/// <remarks>This value is only used for output and has no effect on episode number matching.</remarks>
+		public static int EpisodeNumberDigits
+		{
+			get
+			{
+				return episodeNumberDigits;
+			}
+			set
+			{
+				if(value > 5)
+					episodeNumberDigits = 5;
+				else if(value < 1)
+					episodeNumberDigits = 1;
+				else
+					episodeNumberDigits = value;
+			}
+		}
+
+		/// <summary>
+		/// Removes all strings that are <c>null</c>, empty or only contain whitespace from an array, returning a new array.
+		/// </summary>
+		/// <param name="arr">The array from which to remove the strings.</param>
+		/// <returns>A new array containing only not empty strings.</returns>
+		public static string[ ] RemoveEmptyStrings(string[ ] arr)
+		{
+			int count = 0;
+
+			foreach(string temp in arr)
+			{
+				if(string.IsNullOrWhiteSpace(temp))
+					count++;
+			}
+
+			string[ ] newArray = new string[arr.Length - count];
+			count = 0;
+
+			foreach(string temp in arr)
+			{
+				if(!string.IsNullOrWhiteSpace(temp))
+				{
+					newArray[count] = temp;
+					count++;
+				}
+			}
+
+			return newArray;
+		}
+
+		/// <summary>
+		/// Formats a filename from a given series name, episode number and file extension according to the values 
+		/// of <see cref="Separator"/>, <see cref="SeasonPrefix"/> and <see cref="EpisodePrefix"/> set.
+		/// </summary>
+		/// <param name="series">The name of the series.</param>
+		/// <param name="episodeNumber">The episode number, the season number being the X- and the episode number being Y-coordinate.</param>
+		/// <param name="extension">The file extension, including the period (e.g. ".avi").</param>
+		/// <returns>The filename corresponding to the input values.</returns>
+		public static string FormatFilename(string series, Point episodeNumber, string extension)
+		{
+			return FormatFilename(series, episodeNumber, "", extension);
+		}
+
+		/// <summary>
+		/// Formats a filename from a given series name, episode number, episode name and file extension according to the values 
+		/// of <see cref="Separator"/>, <see cref="SeasonPrefix"/> and <see cref="EpisodePrefix"/> set.
+		/// </summary>
+		/// <param name="series">The name of the series.</param>
+		/// <param name="episodeNumber">The episode number, the season number being the X- and the episode number being Y-coordinate.</param>
+		/// <param name="episodeName">The episode name.</param>
+		/// <param name="extension">The file extension, including the period (e.g. ".avi").</param>
+		/// <returns>The filename corresponding to the input values.</returns>
+		public static string FormatFilename(string series, Point episodeNumber, string episodeName, string extension)
+		{
+			StringBuilder sb = new StringBuilder(series.Trim());
+			sb.Append(Separator).Append(SeasonPrefix);
+			sb.Append(episodeNumber.X.ToString("D" + EpisodeNumberDigits.ToString())).Append(EpisodePrefix);
+			sb.Append(episodeNumber.Y.ToString("D" + EpisodeNumberDigits.ToString()));
+			if(!string.IsNullOrWhiteSpace(episodeName))
+				sb.Append(Separator).Append(episodeName.Trim());
+			sb.Append(extension.Trim());
+
+			foreach(char c in Path.GetInvalidFileNameChars())
+				sb.Replace(c.ToString(), "");
+			foreach(char c in Path.GetInvalidPathChars())
+				sb.Replace(c.ToString(), "");
+			sb.Replace("  ", " ");
+			sb.Replace("..", ".");
+			sb.Replace("__", "_");
+
+			return sb.ToString().Trim();
+		}
+
+		/// <summary>
+		/// Extracts episode information, consisting of season number and episode number, from a filename or part of a filename.
+		/// </summary>
+		/// <param name="part">The filename or part of a filename to extract the episode information from.</param>
+		/// <returns>On success, a <see cref="System.Drawing.Point"/> structure with X and Y coordinates set to season and episode number respectively.
+		/// On failure, a <see cref="System.Drawing.Point"/> structure with both X and Y coordinates set to <c>-1</c>.</returns>
+		public static Point GetEpisodeInfo(string part)
+		{
+			Match m = Regex.Match(part, RegexEpisodeNumberMatchString, DefaultRegexOptions);
+
+			if(m.Success)
+			{
+				string[ ] parts = Regex.Split(m.Value, @"(?<=[0-9])[^0-9]+(?=[0-9])", RegexOptions.Singleline);
+
+				if(parts.Length == 2)
+					try
+					{
+						int x = int.Parse(parts[0]);
+						int y = int.Parse(parts[1]);
+
+						return new Point(x, y);
+					}
+					catch
+					{
+						return new Point(-1, -1);
+					}
+			}
+
+			return new Point(-1, -1);
+		}
+
+		#endregion static members
+	}
+}
