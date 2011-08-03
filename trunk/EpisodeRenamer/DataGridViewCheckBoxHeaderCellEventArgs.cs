f716430b7c4c145539103e7a13b49dbdf85b18a0
Peter <Peter@e3f225bf-7ebb-8b45-85b9-207a5bdb3f18>
2011-08-03 11:31:38 +0000

    EpisodeRenamer: -> Added DatagridViewCheckBoxHeaderCell and DataGridViewCheckBoxHeaderCellEventArgs

---
+++ trunk/EpisodeRenamer/DataGridViewCheckBoxHeaderCellEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EpisodeRenamer
+{
+	public class DataGridViewCheckBoxHeaderCellEventArgs : EventArgs
+	{
+		bool _bChecked;
+
+		public DataGridViewCheckBoxHeaderCellEventArgs(bool bChecked)
+		{
+			_bChecked = bChecked;
+		}
+
+		public bool Checked
+		{
+			get
+			{
+				return _bChecked;
+			}
+		}
+	}
+}
