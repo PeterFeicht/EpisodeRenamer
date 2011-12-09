b4f6bb0333e34c432ade014806a2e0f06ad9bed1
Peter <Peter@e3f225bf-7ebb-8b45-85b9-207a5bdb3f18>
2011-12-09 12:56:09 +0000

    EpisodeRenamer: -> Fixed console being opened when program isn't started from command line.

--- trunk/EpisodeRenamer/Program.cs
+++ trunk/EpisodeRenamer/Program.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace EpisodeRenamer
 {
 	static class Program
 	{
+		[DllImport("Kernel32.dll")]
+		public static extern bool AttachConsole(int pid);
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main(string[] args)
 		{
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			MainForm frmMain = null;
@@ -24,6 +29,8 @@ namespace EpisodeRenamer
 
 				if(args[0] == "-h" || args[0] == "--help")
 				{
+					AttachConsole(-1);
+					Console.WriteLine();
 					Console.WriteLine("usage: EpisodeRenamer [options]\n\noptions are:\n  -l | --log\n    Create a log file and write debugging information.");
 					Console.WriteLine("  -h | --help\n    Display this help message.");
 					return;
